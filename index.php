32608eb55a370d16a156f4dd209de14caeb04eee
reyestaaa <muhammadadrianreyesta9d@gmail.com>
2021-08-22 17:35:54 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php 
+$url = file_get_contents('https://api.kawalcorona.com/indonesia');
+$data = json_decode($url, true);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update Data Covid-19</title>
+    <link rel="shortcut icon" href="indonesia.svg" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+</head>
+<body>
+    <!-- Image and text -->
+    <nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+    <a class="navbar-brand" href="#">
+    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+    <path fill-rule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+    <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+    </svg>
+        Home
+    </a>
+    </div>
+    </nav>
+
+
+
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col-md-12">
+                <div class="alert alert-primary" role="alert">
+                <strong>Update</strong> Data Virus Corona Indonesia 
+                </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card text-white bg-primary mb-3 p-3 shadow">
+                    <div class="card-body">
+                        <h5 class="card-title text-center"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-frown-fill" viewBox="0 0 16 16">
+  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm-2.715 5.933a.5.5 0 0 1-.183-.683A4.498 4.498 0 0 1 8 9.5a4.5 4.5 0 0 1 3.898 2.25.5.5 0 0 1-.866.5A3.498 3.498 0 0 0 8 10.5a3.498 3.498 0 0 0-3.032 1.75.5.5 0 0 1-.683.183zM10 8c-.552 0-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5S10.552 8 10 8z"/>
+</svg> Positif</h5>
+                        <p class="card-text text-center"><?= $data[0]['positif'] ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card text-white bg-warning mb-3 p-3 shadow">
+                    <div class="card-body">
+                        <h5 class="card-title text-center"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-frown-fill" viewBox="0 0 16 16">
+  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm-2.715 5.933a.5.5 0 0 1-.183-.683A4.498 4.498 0 0 1 8 9.5a4.5 4.5 0 0 1 3.898 2.25.5.5 0 0 1-.866.5A3.498 3.498 0 0 0 8 10.5a3.498 3.498 0 0 0-3.032 1.75.5.5 0 0 1-.683.183zM10 8c-.552 0-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5S10.552 8 10 8z"/>
+</svg> Dirawat</h5>
+                        <p class="card-text text-center"><?= $data[0]['dirawat'] ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card text-white bg-success mb-3 p-3 shadow">
+                    <div class="card-body">
+                        <h5 class="card-title text-center"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-frown-fill" viewBox="0 0 16 16">
+  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm-2.715 5.933a.5.5 0 0 1-.183-.683A4.498 4.498 0 0 1 8 9.5a4.5 4.5 0 0 1 3.898 2.25.5.5 0 0 1-.866.5A3.498 3.498 0 0 0 8 10.5a3.498 3.498 0 0 0-3.032 1.75.5.5 0 0 1-.683.183zM10 8c-.552 0-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5S10.552 8 10 8z"/>
+</svg> Sembuh</h5>
+                        <p class="card-text text-center"><?= $data[0]['sembuh'] ?></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card text-white bg-danger mb-3 p-3 shadow">
+                    <div class="card-body">
+                        <h5 class="card-title text-center"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-emoji-frown-fill" viewBox="0 0 16 16">
+  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm-2.715 5.933a.5.5 0 0 1-.183-.683A4.498 4.498 0 0 1 8 9.5a4.5 4.5 0 0 1 3.898 2.25.5.5 0 0 1-.866.5A3.498 3.498 0 0 0 8 10.5a3.498 3.498 0 0 0-3.032 1.75.5.5 0 0 1-.683.183zM10 8c-.552 0-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5S10.552 8 10 8z"/>
+</svg> Meninggal</h5>
+                        <p class="card-text text-center"><?= $data[0]['meninggal'] ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+<h3 class="text-center p-5 font-weight-light">Data ini otomatis berubah seiring nya waktu</h3>
+
+</body>
+</html>
